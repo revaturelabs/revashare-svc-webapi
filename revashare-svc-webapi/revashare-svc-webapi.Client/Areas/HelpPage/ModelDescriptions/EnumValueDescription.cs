18897b2b77098c80fd07b43d3d1428eb3860deb3
Harley Wang <harleywwang@gmail.com>
2016-12-12 15:17:13 -0500

    initial commit

---
+++ revashare-svc-webapi/revashare-svc-webapi.Client/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace revashare_svc_webapi.Client.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}

ace7567321c3a6678c197f50e66a9d1987f74848
jason goodell <jason.goodell@outlook.com>
2016-12-13 11:51:54 -0500

    add rider controller and interface [#136036659]

---
+++ revashare-svc-webapi/revashare-svc-webapi.Logic/Models/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace revashare_svc_webapi.Logic.Interfaces
+{
+   public class UserDTO
+   {
+   }
+}

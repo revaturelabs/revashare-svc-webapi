062d6b39c9c735732418e1b4432a218292ec3cf0
Harley Wang <harleywwang@gmail.com>
2016-12-13 11:45:11 -0500

    add driver interface and format controller [#136036659]

---
+++ revashare-svc-webapi/revashare-svc-webapi.Logic/Interfaces/IDriverRepository.cs
@@ -0,0 +1,16 @@
+﻿using revashare_svc_webapi.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace revashare_svc_webapi.Logic.Interfaces {
+  public interface IDriverRepository {
+    bool UpdateVehicleInfo(VehicleDTO vehicle);
+
+    bool ReportRider(RiderDTO rider);
+
+    bool SetAvailability();
+  }
+}

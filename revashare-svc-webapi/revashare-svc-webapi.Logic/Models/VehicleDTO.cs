c1cb3f85666e20acc4340f524d619dc9ad10e6cf
Harley Wang <harleywwang@gmail.com>
2016-12-13 12:53:31 -0500

    update vehicle dto [#135977713]

--- revashare-svc-webapi/revashare-svc-webapi.Logic/Models/VehicleDTO.cs
+++ revashare-svc-webapi/revashare-svc-webapi.Logic/Models/VehicleDTO.cs
@@ -6,5 +6,18 @@ using System.Threading.Tasks;
 
 namespace revashare_svc_webapi.Logic.Models {
   public class VehicleDTO {
+    public int Id { get; set; }
+
+    public int OwnerId { get; set; }
+
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+
+    public string LicensePlate { get; set; }
+
+    public string Color { get; set; }
+
+    public int Capacity { get; set; }
   }
 }

970d6ee86dc6b7a2bdf83c65ef77cdf179a56cdf
Harley Wang <harleywwang@gmail.com>
2016-12-13 10:32:37 -0500

    setup interface folder and driver controller [#136036659]

---
+++ revashare-svc-webapi/revashare-svc-webapi.Client/Controllers/DriverController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace revashare_svc_webapi.Client.Controllers
+{
+    public class DriverController : ApiController
+    {
+        // GET: api/Driver
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Driver/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Driver
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Driver/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Driver/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

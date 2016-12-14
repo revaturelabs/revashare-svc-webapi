e2c5108566431c456fe1547190c3014276ec2950
Harley Wang <harleywwang@gmail.com>
2016-12-14 14:03:31 -0500

    add driver implementation class for interface [#136060481]

--- revashare-svc-webapi/revashare-svc-webapi.Logic/DriverLogic.cs
+++ revashare-svc-webapi/revashare-svc-webapi.Logic/DriverLogic.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using revashare_svc_webapi.Logic.Models;
+using revashare_svc_webapi.Logic.RevaShareServiceReference;
 
 namespace revashare_svc_webapi.Logic {
   public class DriverLogic : IDriverRepository {
+    private RevaShareDataServiceClient svc = new RevaShareDataServiceClient();
+
     public bool ReportRider(FlagDTO flag) {
       throw new NotImplementedException();
     }
@@ -18,6 +21,7 @@ namespace revashare_svc_webapi.Logic {
 
     public bool UpdateVehicleInfo(VehicleDTO vehicle) {
       throw new NotImplementedException();
+      /*return*/
     }
   }
 }

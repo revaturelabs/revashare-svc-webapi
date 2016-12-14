6863c47492fca596a360c4e98053f0b772ae4940
Harley Wang <harleywwang@gmail.com>
2016-12-14 13:30:14 -0500

    add driver logic class with functions [#136060481]

---
+++ revashare-svc-webapi/revashare-svc-webapi.Logic/DriverLogic.cs
@@ -0,0 +1,23 @@
+﻿using revashare_svc_webapi.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using revashare_svc_webapi.Logic.Models;
+
+namespace revashare_svc_webapi.Logic {
+  public class DriverLogic : IDriverRepository {
+    public bool ReportRider(FlagDTO flag) {
+      throw new NotImplementedException();
+    }
+
+    public bool SetAvailability() {
+      throw new NotImplementedException();
+    }
+
+    public bool UpdateVehicleInfo(VehicleDTO vehicle) {
+      throw new NotImplementedException();
+    }
+  }
+}

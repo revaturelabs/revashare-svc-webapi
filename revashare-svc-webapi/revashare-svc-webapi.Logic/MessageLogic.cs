f7e878a96b3e5cb6be85c210b502765730189838
Harley Wang <harleywwang@gmail.com>
2016-12-15 09:54:55 -0500

    add notifications [#136186415]

---
+++ revashare-svc-webapi/revashare-svc-webapi.Logic/MessageLogic.cs
@@ -0,0 +1,19 @@
+﻿using revashare_svc_webapi.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using revashare_svc_webapi.Logic.Models;
+
+namespace revashare_svc_webapi.Logic {
+  public class MessageLogic : IMessageRepository {
+    public bool NotifyDropPassenger(RideDTO ride, UserDTO rider) {
+      throw new NotImplementedException();
+    }
+
+    public bool NotifyNewPassenger(RideDTO ride, UserDTO rider) {
+      throw new NotImplementedException();
+    }
+  }
+}

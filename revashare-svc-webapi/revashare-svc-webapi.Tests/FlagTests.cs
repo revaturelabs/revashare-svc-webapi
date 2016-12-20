02501a04cf43b1e699177b2ca5f46aed308c6d50
matt obrien <obrien.matthew.cs@gmail.com>
2016-12-20 11:45:44 -0500

    Add FlagTests file with working getFlags test [#136389281]

---
+++ revashare-svc-webapi/revashare-svc-webapi.Tests/FlagTests.cs
@@ -0,0 +1,27 @@
+﻿using revashare_svc_webapi.Logic.RevaShareServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace revashare_svc_webapi.Tests
+{
+  
+  public class FlagTests
+  {
+    [Fact]
+    public void test_GetFlags()
+    {
+      RevaShareDataServiceClient dataClient = new RevaShareDataServiceClient();
+
+      List<FlagDAO> getflags = dataClient.GetAllFlags().ToList();
+
+      Assert.NotNull(getflags);
+      
+
+
+    }
+  }
+}

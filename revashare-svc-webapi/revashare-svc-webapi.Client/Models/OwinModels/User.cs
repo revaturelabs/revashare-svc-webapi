7ac7ff8760bcbc53ff9ded5d9750e7432fdfdd44
David Towson <david115632042@gmail.com>
2016-12-22 12:50:21 -0500

    owin models mostly done.

--- revashare-svc-webapi/revashare-svc-webapi.Client/Models/OwinModels/User.cs
+++ revashare-svc-webapi/revashare-svc-webapi.Client/Models/OwinModels/User.cs
@@ -8,6 +8,12 @@ using System.Web;
 
 namespace revashare_svc_webapi.Client.Models.OwinModels
 {
+
+    public class MissingPermission: Exception
+    {
+
+    }
+
     public class User : ClaimsPrincipal
     {
 
@@ -40,101 +46,257 @@ namespace revashare_svc_webapi.Client.Models.OwinModels
 
         public virtual bool requestToBeDriver(DriverEnrollDTO enroll)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
+        #region rider methods
         public virtual List<ApartmentDTO> getApartments()
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual ApartmentDTO getApartmentByName(string name)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual bool fileComplaintAboutDriver(FlagDTO flag)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual List<RideDTO> getAvailableRides(string startLocation)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual List<RideDTO> getRides()
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual int getAvailableSeatsInRide(RideDTO ride)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual RideDTO getCurrentSelectedRide(RideDTO ride)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual bool addRiderToRide(RideDTO ride, UserDTO rider)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual bool removeRiderFromRide(UserDTO rider, RideDTO ride)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual List<RideRiderDTO> getRideRidersByUser(UserDTO rider)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual List<RideRiderDTO> getRideRiders()
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual List<UserDTO> GetRidersByRide(RideDTO ride)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual int getOccupiedSeatsByRide(RideDTO ride)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual List<UserDTO> getUsers()
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual bool modifyUserInfo(UserDTO user)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual List<VehicleDTO> getVehicles()
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
         }
 
         public virtual VehicleDTO getVehicleByOwner(UserDTO owner)
         {
-            throw new NotImplementedException();
+            throw new MissingPermission();
+        }
+        #endregion
+
+        #region driver methods 
+        public virtual VehicleDTO ViewVehicleInfo(string driver)
+        {
+            throw new MissingPermission();
         }
 
+        public virtual bool AddVehicle(VehicleDTO vehicle)
+        {
+            throw new MissingPermission();
+        }
 
+        public virtual bool UpdateVehicleInfo(VehicleDTO vehicle)
+        {
+            throw new MissingPermission();
+        }
 
+        public virtual bool ReportRider(FlagDTO flag)
+        {
+            throw new MissingPermission();
+        }
 
+        public virtual bool SetAvailability(RideDTO ride)
+        {
+            throw new MissingPermission();
+        }
 
+        public virtual bool UpdateDriverProfile(UserDTO driver)
+        {
+            throw new MissingPermission();
+        }
 
+        public virtual bool ScheduleRide(RideDTO ride)
+        {
+            throw new MissingPermission();
+        }
 
+        public virtual RideDTO getSingleRide(RideDTO ride)
+        {
+            throw new MissingPermission();
+        }
 
+        public virtual bool CancelRide(RideDTO ride)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual List<UserDTO> ViewPassengers(RideDTO ride)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool AcceptPassenger(RideRiderDTO rider)
+        {
+            throw new MissingPermission();
+        }
+
+        #endregion
+
+        #region admin methods
+
+        public virtual bool InsertAdmin(UserDTO adminToAdd)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual List<UserDTO> RequestAdmins()
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual UserDTO RequestAdmin(string UserName)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool ModifyAdmin(UserDTO adminToUpdate)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool DeleteAdmin(string UserName)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool ApproveUser(UserDTO userToApprove)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool ApproveDriver(UserDTO driverToApprove)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual List<UserDTO> GetPendingRiders()
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual List<UserDTO> GetPendingDrivers()
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual UserDTO RequestDriver(string UserName)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual List<UserDTO> RequestDrivers()
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool ModifyDriver(UserDTO driverToUpdate)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool DeleteDriver(UserDTO driverToRemove)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual List<FlagDTO> GetUserReports()
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool RemoveReport(FlagDTO reportToRemove)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool RemoveDriverPrivileges(FlagDTO reportForUser)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual UserDTO RequestRider(string UserName)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual List<UserDTO> RequestRiders()
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool ModifyRider(UserDTO riderToUpdate)
+        {
+            throw new MissingPermission();
+        }
+
+        public virtual bool DeleteRider(UserDTO riderToRemove)
+        {
+            throw new MissingPermission();
+        }
+
+        #endregion
 
 
 
